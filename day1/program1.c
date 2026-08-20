54949a40be3eecb3279c8eb275771ecd3a03bab1
rumangaur-tech <rumangaur@gmail.com>
2026-08-20 22:21:57 +0530

    Day 1: Add C programs

---
+++ day1/program1.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main() {
+  int a,b,sum;
+  printf("Enter two numbers:");
+  scanf("%d%d",&a,&b);
+  printf("First number=%d\n",a);
+  printf("Second number=%d\n",b);
+sum=a+b;
+printf("%d",sum);
+  return 0;
+}
