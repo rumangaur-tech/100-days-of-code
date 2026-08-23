aa15c93122f0a7e39dd9724b54b2550db5c0079a
rumangaur-tech <rumangaur@gmail.com>
2026-08-24 00:02:10 +0530

    Add Day2 circle program

---
+++ day2.c
@@ -0,0 +1,14 @@
+
+#include<stdio.h>
+
+int main(){
+ float radius,area,circumference;
+ float pi = 3.14;
+ printf("Enter radius of circle:");
+ scanf("%f",&radius);
+ area = pi*radius*radius;
+ circumference = 2*pi*radius;
+ printf("Area = %.2f\n",area);
+ printf("circumference = %.2f\n",circumference);
+ return 0;
+}
