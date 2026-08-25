93c4f99803baed0b7cf04d215833393e568929d5
rumangaur-tech <rumangaur@gmail.com>
2026-08-25 22:54:23 +0530

    Add Day5 interest program

---
+++ day5/program1.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+    float principal, rate, time;
+    float simple_interest, compound_interest;
+
+    printf("Enter principal: ");
+    scanf("%f", &principal);
+
+    printf("Enter rate: ");
+    scanf("%f", &rate);
+
+    printf("Enter time: ");
+    scanf("%f", &time);
+
+    simple_interest = (principal * rate * time) / 100;
+
+    compound_interest = principal * pow((1 + rate / 100), time) - principal;
+
+    printf("Simple Interest = %.2f\n", simple_interest);
+    printf("Compound Interest = %.2f\n", compound_interest);
+
+    return 0;
+}
