0046d4f904be3a29c3baa5c10fe0a16b1f8aeca7
rumangaur-tech <rumangaur@gmail.com>
2026-09-04 23:13:14 +0530

    Add Day20 program2

---
+++ day20/program2.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main()
+{
+    char binary[100];
+    int i;
+
+    printf("Enter a binary number: ");
+    scanf("%s", binary);
+
+    for (i = 0; binary[i] != '\0'; i++)
+    {
+        if (binary[i] == '0')
+            binary[i] = '1';
+        else if (binary[i] == '1')
+            binary[i] = '0';
+    }
+
+    printf("1's complement = %s\n", binary);
+
+    return 0;
+}
