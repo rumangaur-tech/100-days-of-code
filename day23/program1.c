fb95cda0af45344f4fe2ebcd91ee2a07d1b59879
rumangaur-tech <rumangaur@gmail.com>
2026-09-08 22:10:49 +0530

    Day23 sum of series program

---
+++ day23/program1.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main() {
+int n,i;
+double sum = 0.0;
+printf("Enter number of terms:");
+scanf("%d",&n);
+for(i = 1;i<=n;i++){
+sum = sum + (double)(2*i)/(4*i-1);
+}
+printf("Sum of the series = %.2lf\n",sum);
+return 0;
+}
