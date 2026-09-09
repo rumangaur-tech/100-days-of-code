9cee2d7930f73856c1c0c89a3d730f9ed219f40d
rumangaur-tech <rumangaur@gmail.com>
2026-09-09 22:36:44 +0530

    Day29 maximum and minimum in array

---
+++ day29/program2.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+int main()
+{
+    int arr[100], n, i;
+    int max, min;
+
+    printf("Enter the number of elements: ");
+    scanf("%d", &n);
+
+    printf("Enter the elements:\n");
+
+    for (i = 0; i < n; i++)
+    {
+        scanf("%d", &arr[i]);
+    }
+
+    max = arr[0];
+    min = arr[0];
+
+    for (i = 1; i < n; i++)
+    {
+        if (arr[i] > max)
+            max = arr[i];
+
+        if (arr[i] < min)
+            min = arr[i];
+    }
+
+    printf("Maximum = %d\n", max);
+    printf("Minimum = %d\n", min);
+
+    return 0;
+}
