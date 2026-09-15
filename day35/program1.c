18cac9887c2218cf9439d6a52bcbde7efffc2a90
rumangaur-tech <rumangaur@gmail.com>
2026-09-15 23:32:21 +0530

    Day35 program1

---
+++ day35/program1.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+int main() {
+    int n, i;
+    int arr[100];
+    int largest, secondLargest;
+
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+
+    printf("Enter elements:\n");
+    for (i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    largest = secondLargest = -999999;
+
+    for (i = 0; i < n; i++) {
+        if (arr[i] > largest) {
+            secondLargest = largest;
+            largest = arr[i];
+        }
+        else if (arr[i] > secondLargest && arr[i] != largest) {
+            secondLargest = arr[i];
+        }
+    }
+
+    printf("Second largest element = %d\n", secondLargest);
+
+    return 0;
+}
