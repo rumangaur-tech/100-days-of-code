b0bf4eeb2adea948050303a0e61b0d57eaeb7bfb
rumangaur-tech <rumangaur@gmail.com>
2026-09-16 12:26:20 +0530

    Day37 program2

---
+++ day37/program2.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+int main() {
+    int a[10][10], transpose[10][10];
+    int r, c, i, j;
+
+    printf("Enter number of rows: ");
+    scanf("%d", &r);
+
+    printf("Enter number of columns: ");
+    scanf("%d", &c);
+
+    printf("Enter matrix elements:\n");
+
+    for (i = 0; i < r; i++) {
+        for (j = 0; j < c; j++) {
+            scanf("%d", &a[i][j]);
+        }
+    }
+
+    // Find transpose
+    for (i = 0; i < r; i++) {
+        for (j = 0; j < c; j++) {
+            transpose[j][i] = a[i][j];
+        }
+    }
+
+    printf("Transpose of the matrix:\n");
+
+    for (i = 0; i < c; i++) {
+        for (j = 0; j < r; j++) {
+            printf("%d ", transpose[i][j]);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
