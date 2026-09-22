f16d384dca9bbeb50cbd2e4400efd74cb1f7222b
rumangaur-tech <rumangaur@gmail.com>
2026-09-22 19:55:02 +0530

    Day43 program1

---
+++ day43/program1.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main() {
+    char str[100];
+    int i, length = 0;
+
+    printf("Enter a string: ");
+    fgets(str, sizeof(str), stdin);
+
+    // Find length
+    while (str[length] != '\0' && str[length] != '\n') {
+        length++;
+    }
+
+    // Print in reverse
+    printf("Reversed string: ");
+
+    for (i = length - 1; i >= 0; i--) {
+        printf("%c", str[i]);
+    }
+
+    printf("\n");
+
+    return 0;
+}
