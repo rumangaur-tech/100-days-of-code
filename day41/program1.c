875a0880e6be6bcee00d45fef623d0bcaffd0f65
rumangaur-tech <rumangaur@gmail.com>
2026-09-22 19:42:38 +0530

    Day 41 program1

---
+++ day41/program1.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main() {
+    char str[100];
+    int count = 0;
+
+    printf("Enter a string: ");
+    fgets(str, sizeof(str), stdin);
+
+    while (str[count] != '\0') {
+        if (str[count] != '\n') {
+            count++;
+        } else {
+            break;
+        }
+    }
+
+    printf("Number of characters = %d\n", count);
+
+    return 0;
+}
